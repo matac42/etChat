fc2badf85aac9e12afb6454ad602ce03403b2b6f
root <root@ip-172-31-43-235.us-east-2.compute.internal>
2020-05-25 13:07:11 +0000

    create database connection at main.go

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	
+	"time"
+	_"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
+
 
 type melodyHandler struct {
 	melo *melody.Melody
@@ -98,6 +103,26 @@ func getAccessTokenClient(c *gin.Context) {
 }
 
 func main() {
+
+	// db接続
+    db, err := sqlConnect()
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+    error := db.Create(&Users{
+        Name:     "testtarou",
+        Age:      18,
+        Address:  "tokyo",
+        UpdateAt: getDate(),
+    }).Error
+    if error != nil {
+        fmt.Println(error)
+    } else {
+        fmt.Println("データ追加成功")
+    }	
+		
 	r := gin.Default() //ginは基本的にgin.Default()の返す構造体のメソッド経由でないと操作できない．
 	r.LoadHTMLGlob("html/*.html")
 
@@ -112,4 +137,33 @@ func main() {
 		v1.GET("callback", getAccessTokenClient)
 	}
 	r.Run(":8080")
+}
+
+
+
+
+func getDate() string {
+    const layout = "2006-01-02 15:04:05"
+    now := time.Now()
+    return now.Format(layout)
+}
+
+// SQLConnect DB接続
+func sqlConnect() (database *gorm.DB, err error) {
+    DBMS := "mysql"
+    USER := "jb5"
+    PASS := "h19life"
+    PROTOCOL := "tcp(localhost:3306)"
+    DBNAME := "et"
+
+    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+    return gorm.Open(DBMS, CONNECT)
+}
+// Users ユーザー情報のテーブル情報
+type Users struct {
+    ID       int
+    Name     string `json:"name"`
+    Age      int    `json:"age"`
+    Address  string `json:"address"`
+    UpdateAt string `json:"updateAt" sql:"not null;type:date"`
 }
